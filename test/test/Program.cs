f89387fbfd4a100b4ec95f3d5d285fd8e68bd596
Dbehan1998 <deb2610@g.rit.edu>
2017-02-21 17:15:06 -0500

    added again

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -12,7 +12,7 @@ namespace test
         {
             for(int i = 0; i < 1000; i++)
             {
-                Console.WriteLine("BURN THE WORLD BURN IT ALL");
+                Console.WriteLine("BURN THE WORLD BURN IT ALL AGAIN!!!");
             }
         }
     }
