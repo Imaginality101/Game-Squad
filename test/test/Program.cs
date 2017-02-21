b950118d761c5ad17a3b5c2f6839b2823b009deb
Tom <tps3871@g.rit.edu>
2017-02-21 17:11:09 -0500

    test

---
+++ test/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("BURN THE WORLD BURN IT ALL");
+            }
+        }
+    }
+}
