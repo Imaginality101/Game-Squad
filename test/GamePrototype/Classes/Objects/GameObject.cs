9c559d2180df4efa630fdb10a6b1bf845a4a9383
Caleb <Caleb@PC01>
2017-02-25 17:39:55 -0500

    I did the TODO in the GameObject class. I added another TODO when we make the player.

---
+++ test/GamePrototype/Classes/Objects/GameObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace GamePrototype.Classes.Objects
+{
+    class GameObject
+    {
+        private Texture2D sprite;
+        private Vector2 position;
+        private Rectangle hitBox;
+
+        public GameObject()
+        {
+            sprite = null;
+            position = new Vector2(0, 0);
+            
+        }
+
+        public GameObject(Texture2D txtr, Vector2 strtPs)
+        {
+            sprite = txtr;
+            position = strtPs;
+
+        }
+        //TODO: Needs accessor properties for Sprite and Position
+        //NOTE: CALEB - I'll try - I assume read only for sprite, and I will see id I
+        // can have a set for Position that only affects the player
+        
+        // read only sprite property
+        public Texture2D Sprite
+        {
+            get
+            {
+                return sprite;
+            }
+        }
+
+        // property for position; can only be set if GameObject is player
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                // TODO: once the Player class is added, include this if statement:
+                // if (this is Player)
+                //{
+                position = value;
+                //} 
+            }
+        }
+
+
+    }
+}
---
+++ test/GamePrototype/Classes/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GamePrototype.Classes.Objects;
+
+namespace GamePrototype.Classes
+{
+    class Room
+    {
+        private SpriteBatch spritesToDrawInYoMama;
+        private GameObject[] objectsInYoMama;
+
+
+    }
+}
