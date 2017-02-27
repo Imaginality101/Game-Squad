44d7b8ed7b7242afbd3a9fb223514c87850d1cc4
Tom <tps3871@g.rit.edu>
2017-02-27 17:00:52 -0500

    menu still has like nothing but it has comments now ; w;

--- test/GamePrototype/Classes/Menu/Menu.cs
+++ test/GamePrototype/Classes/Menu/Menu.cs
@@ -8,5 +8,12 @@ namespace GamePrototype.Classes.Menu
 {
     class Menu
     {
+
     }
+
+    // TODO: Menu class will need an update and draw method, should check for input
+    // Needs to have a way of tracking which menu item it's on, we may also need a more categorical version of this class to hold other menus
+    // If the player hits whatever the method of selection is, it should call an Open() method of the menu item highlighted
+    // The menu view itself also needs a property and attribute to check if it's enabled, as well as an Open method for itself which toggles that on/Close method for off
+    // Other than the main view of the phone itself and maybe settings, menus should use lists for content since as the player finds clues they go there
 }
