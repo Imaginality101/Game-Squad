6fc274c71f1ec1f5d31c01203669659e3bc84d61
Tom <tps3871@g.rit.edu>
2017-02-28 17:04:22 -0500

    gah

--- test/GamePrototype/Classes/Menu/Menu.cs
+++ test/GamePrototype/Classes/Menu/Menu.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace GamePrototype.Classes.Menu
 {
-    class Menu
+    class Menu : Tools.IControlled
     {
 
+        public void CheckInput()
+        {
+
+        }
     }
 
     // TODO: Menu class will need an update and draw method, should check for input
@@ -16,4 +20,5 @@ namespace GamePrototype.Classes.Menu
     // If the player hits whatever the method of selection is, it should call an Open() method of the menu item highlighted
     // The menu view itself also needs a property and attribute to check if it's enabled, as well as an Open method for itself which toggles that on/Close method for off
     // Other than the main view of the phone itself and maybe settings, menus should use lists for content since as the player finds clues they go there
+    // Implements IControlled, use this to check user input while in the menu. Whoever does player updating should make sure they check if the menu is open before checking for input.
 }
