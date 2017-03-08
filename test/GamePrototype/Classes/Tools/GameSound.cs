175ef0f3c2d85836d8082b70099f9498a02c0070
Dbehan1998 <deb2610@g.rit.edu>
2017-03-08 10:14:39 -0500

    GameSound Class created to hand out music easily.

---
+++ test/GamePrototype/Classes/Tools/GameSound.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace GamePrototype.Classes.Tools
+{
+    //TODO: This class handles the sound effect building and the backround music for the game
+    //Declan Behan
+    class GameSound
+    {
+        //Attributes
+        SoundEffect singleEffect;
+        SoundEffectInstance singleEffectInst;
+        //Constructor
+        public GameSound(string filename, ContentManager content)
+        {
+            
+            singleEffect = content.Load<SoundEffect>(filename);
+            singleEffectInst = singleEffect.CreateInstance();
+
+        }
+        //PlayAsSoundEffect Method - Takes a float volume as a parameter
+        public void PlayAsSoundEffect(float volume0to1f)
+        {
+            singleEffectInst = singleEffect.CreateInstance();
+            singleEffectInst.Play();
+            singleEffectInst.Volume = volume0to1f;
+        }
+
+        //PlayAsMusic Method - Takes a float volume as a parameter
+        public void PlayAsMusic(float volume0to1f)
+        {
+            if (singleEffectInst.State == SoundState.Stopped)
+            {
+                singleEffectInst.Play();
+                singleEffectInst.Volume = volume0to1f;
+            }
+        }
+        //EndMusic Method - stops the music
+        public void EndMusic()
+        {
+            singleEffectInst.Stop();
+        }
+    } 
+}
