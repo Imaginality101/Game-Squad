4c65e7ae9b8c44135b014b1011d1b962d3529c61
Dbehan1998 <deb2610@g.rit.edu>
2017-03-26 16:35:59 -0400

    adding comments

--- test/GamePrototype/Classes/Tools/ObjectSetup.cs
+++ test/GamePrototype/Classes/Tools/ObjectSetup.cs
@@ -15,17 +15,7 @@ namespace GamePrototype.Classes.Tools
 {
     class ObjectSetup
     {
-        // TODO: Declan - You can use this for initializing. I'll give some recommendations but largely leave decisions on methodology up to you.
-        // Right now this is a static class (quick refresher, can't be instantiated), since we don't really need to initialize
-        // it for functionality. What I'd recommend doing is setting up one method for each room we add 
-        // (you could even do a property if you wanted, that'd be a really long property though)
-        // and setting it up to return a list of GameObjects specifically tailored to that room's needs. Then just call that when instantiating
-        // a room.
-        // You could also set it up like TryParse, with a Room as a parameter with the keyword out in front.
-        // Obviously you can also just get rid of the static identifier if you need to.
-
-        // As an important other note, you'll need a way to get all the Texture2Ds you need for the objects. I'd recommend working with Kat,
-        // get a dictionary up and running of Texture2D's with a string key
+        // TODO: Declan - You can use this for initializing game objects
 
         //Texture2Ds                           //Rectangles;
         Texture2D bed;                         Rectangle bedRect;
@@ -55,6 +45,7 @@ namespace GamePrototype.Classes.Tools
             origin = new Vector2(graphics.Viewport.Width / 2, graphics.Viewport.Height / 2);
 
         }
+        //method to setup the objects int the bedroom it returns a list of gameobjects
         public List<GameObject> BedroomSetup()
         {
             // GameObject List for return
