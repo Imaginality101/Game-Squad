7ecbc3b322653a3d41c1c3e88ba6b720a6c4e666
Caleb <Caleb@PC01>
2017-04-03 22:07:07 -0400

    Rudimentary classes for menu

--- test/GamePrototype/Classes/Menu/MenuObject.cs
+++ test/GamePrototype/Classes/Menu/MenuObject.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GamePrototype.Classes;
+using GamePrototype.Classes.Objects;
+using GamePrototype.Classes.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,10 +19,16 @@ namespace GamePrototype.Classes.Menu
     {
         // May need to be separated into multiple other classes
 
+        // attributes
+        Rectangle rect;
         // NOTE: This class may not need to be used in light of how recent menu implementation has been handled, but just in case it's here to cover bases.
         // If we do use it, the primary function will be to associate the clues a player has found with a class that encompasses more. So if the player looks
         // in a menu to see what clues they've found, those'll be associated with MenuObjects which will, more than just containing the Clue object itself, can also have other elements like a thumbnail or such.
         // If we go with this method we'll also use this class to standardize display of menu items in a way that makes it easy to add more and control which appear on the screen, since not obviously not every clue can fit on one
         // "page" of the menu. There's too many and more will be available as the player finds them, so be it a scroll or pages we'll work that out here.
+        public MenuObject(Rectangle r)
+        {
+            rect = r;
+        }
     }
 }
---
+++ test/GamePrototype/Classes/Menu/TextRow.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GamePrototype.Classes;
+using GamePrototype.Classes.Objects;
+using GamePrototype.Classes.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrototype.Classes.Menu
+{
+    class TextRow : MenuObject
+    {
+        // attributes
+        Vector2 position;
+        string text;
+        // constructor
+        public TextRow(Vector2 pos, string t) : base(new Rectangle(pos.ToPoint(), Point.Zero))
+        {
+            text = t;
+        }
+    }
+}
