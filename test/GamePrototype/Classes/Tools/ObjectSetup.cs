07d2b3cfd27ed9dbdbbbc7973df724510411f320
Tom <tps3871@g.rit.edu>
2017-04-06 17:42:50 -0400

    it puts the lotion in the basket because interaction is new and improved

--- test/GamePrototype/Classes/Tools/ObjectSetup.cs
+++ test/GamePrototype/Classes/Tools/ObjectSetup.cs
@@ -82,6 +82,9 @@ namespace GamePrototype.Classes.Tools
             objs.Add(new ClueObject(news1, news1Rect, Clue.Clues["News1"], false, "News 1",true));
             objs.Add(new GameObject(dress, dressRect, "Dresser"));
             objs.Add(new ClueObject(stickynote, stickynoteRect, Clue.Clues["StickyNote"], false, "Sticky Note",true));
+
+            // Setting up interaction points, this is an example on how
+            ((ClueObject)objs[5]).InteractionPoint = new Vector2(30, bedRect.Height / 2); // bed
             return objs;
         }
     }
