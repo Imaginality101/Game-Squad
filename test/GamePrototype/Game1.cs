aa092cc3f73b00b7ff504b5317a4eb148d1b219c
Kat Hanlon <hanlon.kathryn@gmail.com>
2017-04-11 17:03:26 -0400

    fixed my thing

--- test/GamePrototype/Game1.cs
+++ test/GamePrototype/Game1.cs
@@ -362,6 +362,11 @@ namespace GamePrototype
                     bedRoom.Draw(uSpriteBatch);
                     player.Draw(uSpriteBatch);
                 }
+
+                if (bedRoom.LightsOff == true)
+                {
+                    uSpriteBatch.Draw(blacklight, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+                }
             }
 
 
@@ -416,11 +421,6 @@ namespace GamePrototype
             menu.Draw(uSpriteBatch);
             // Draw textbox
             //box.Draw(uSpriteBatch);
-
-            if (bedRoom.LightsOff == true)
-            {
-                uSpriteBatch.Draw(blacklight, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
-            }
 
             // end spritebatch
             uSpriteBatch.End();
