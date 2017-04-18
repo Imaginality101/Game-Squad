8a40414ba14889683beff7b013f96f4b6e5f7a03
Tom <tps3871@g.rit.edu>
2017-04-18 19:09:39 -0400

    death

--- test/GamePrototype/Classes/Objects/Interactable.cs
+++ test/GamePrototype/Classes/Objects/Interactable.cs
@@ -18,6 +18,7 @@ namespace GamePrototype.Classes.Objects
         // attributes - should have a Clue that the player needs to access it
         private Clue keyRequired;
         private Boolean flaggedForUse;
+        Vector2 interactionPoint;
 
         public Interactable(Texture2D txtr, Rectangle psRct) : base(txtr, psRct)
         {
@@ -68,6 +69,18 @@ namespace GamePrototype.Classes.Objects
             {
                 base.Draw(sprtBtch);
             }
+        }
+
+        // property for point of interaction
+        public Vector2 InteractionPoint
+        {
+            get { return interactionPoint; }
+            set { interactionPoint = value; }
+        }
+        public Vector2 GetGlobalInteractPoint()
+        {
+            Vector2 location = GlobalBounds.Location.ToVector2() + interactionPoint;
+            return location;
         }
     }
 }
