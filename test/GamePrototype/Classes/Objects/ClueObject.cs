ba6346b5b16eba51c837efee839a917b55a0c12b
Kat Hanlon <hanlon.kathryn@gmail.com>
2017-05-03 10:41:21 -0400

    the box not working works and other near objects

--- test/GamePrototype/Classes/Objects/ClueObject.cs
+++ test/GamePrototype/Classes/Objects/ClueObject.cs
@@ -111,6 +111,7 @@ namespace GamePrototype.Classes.Objects
             // if there isn't a required clue
             else
             {
+                user.SendMessage("fuck a duck");
                 if (Enabled && !pickedUp)
                 {
                     Console.WriteLine(givenClue.ToString());
